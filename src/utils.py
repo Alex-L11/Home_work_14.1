d7e5524a0bd8598f5a73b8326ce46d3420369885
Alex <hopiru@mail.ru>
2026-09-25 12:47:22 +0300

    в модуле utils изменены аннотации типов в функциях согласно mypy

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,22 @@
 import os
 import json
 
+from typing import cast
 from src.main import Product, Category
 
-def read_json(path: str) -> dict:
+
+def read_json(path: str) -> list[dict]:
     """Открывает JSON-файл"""
     full_path = os.path.abspath(path)
     with open(full_path, 'r', encoding='UTF-8') as file:
         data = json.load(file)
-    return data
+
+    return cast(list[dict], data)
 
 
-def created_objects_from_json(data: dict) -> list:
-    """Создает список по категориям и товарам"""
-    products =[]
+def created_objects_from_json(data: list[dict]) -> list:
+    """ Создает список по категориям и товарам """
+    products = []
     for prod in data:
         cats = []
         for cat in prod['products']:
@@ -23,9 +26,10 @@ def created_objects_from_json(data: dict) -> list:
 
     return products
 
+
 if __name__ == '__main__':
     raw_data = read_json('../data/products.json')
     product_data = created_objects_from_json(raw_data)
+
     print(product_data[0].name)
     print(product_data[0].products)
-
